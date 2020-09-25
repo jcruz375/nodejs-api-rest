f7173b94e7eb8204e063bb5c3635834317e3479d
jcruz375 <joaocruz375@gmail.com>
2020-09-25 20:07:15 -0300

    primeiro commit

---
+++ controllers/atendimento.js
@@ -0,0 +1,31 @@
+const Atendimento = require('../models/atendimento')
+
+module.exports = app => {
+    app.get('/atendimento', (req,res)=> {
+        Atendimento.lista(res)
+    });
+    app.get('/atendimento/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        Atendimento.busca(id, res)
+    });
+    app.post('/atendimento', (req,res)=> {
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento, res);
+    });
+
+    app.patch('/atendimento/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+
+        Atendimento.atualiza(id,valores, res)
+    });
+
+    app.delete('/atendimento/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        
+        Atendimento.deleta(id, res);
+    });
+
+
+};
